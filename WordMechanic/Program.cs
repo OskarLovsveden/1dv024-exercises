af7a83e1cafa5c8597499fa683ef08676d0ef7c9
OskarLovsveden <ol222hf@student.lnu.se>
2020-03-25 20:21:07 +0100

    exercises

---
+++ WordMechanic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordMechanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ WordMechanic/TextAnalyzer.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace WordMechanic
+{
+    public static class TextAnalyzer
+    {
+        public static string[] ExtractWords(string line)
+        {
+
+        }
+
+        public static string[] ExtractWords(string[] lines)
+        {
+
+        }
+
+        public static IDictionary<string, int> GetWordFrequency(string[] words)
+        {
+
+        }
+    }
+}
